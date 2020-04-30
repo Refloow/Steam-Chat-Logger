1a2f13ef86357ae77844bbee2d49948a4967b1e0
Refloow <59449926+Refloow@users.noreply.github.com>
2020-04-30 11:34:01 +0200

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,137 @@
+// Steam Chat Logger - Bot built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowlibrarycontact@gmail.com
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+ */
+
+// Checking if required modules are properly installed
+
+try {
+    // Checking if steam-totp module is installed
+	SteamUser = require('steam-user');
+    // Checking if steam-user module is installed
+	SteamTotp = require('steam-totp');
+    // Checking if fs module is installed
+	fs = require('fs');
+    // Checking if module colors is correctly installed
+	colors = require('colors');
+} catch (ex) {
+    // If modules are not installed showing an clear error message to user.
+	console.log('| [Modules] |: Missing dependencies. Install a version with dependecies or use npm install.');
+	console.log(ex);
+	process.exit(1);
+}
+
+let info;
+let users = {};
+let userLogs = {};
+let chatLogs = "";
+let userMsgs = {}
+
+// Importing files into project
+
+// Importing config file
+const config = require('../Settings/config.js');
+// Importing infolog (file with colors for logs)
+const infolog = require('./infolog.js');
+// Importing methods file
+const method = require('./methods');
+// Client
+const refloow = new SteamUser();
+// Login options if 2fa code generation is enabled
+const LogOnOptionsAUTO = {
+	accountName: config.loginAccName,
+	password: config.password,
+	twoFactorCode: SteamTotp.generateAuthCode(config.shared_secret)
+}
+// Login options if 2fa code generation is disabled
+const LogOnOptionsMANUAL = {
+	accountName: config.loginAccName,
+	password: config.password,
+}
+
+// Method for disabling update notifications
+if(method.DisableUpdateNotif()) {
+    // Checking for correct version (updates) for bot on github
+    method.check();
+}
+
+// APP START
+
+// Loging in if auto generation is enabled
+if(method.AutoGenerateLoginCodes())
+    {
+    refloow.logOn(LogOnOptionsAUTO);
+}
+// Loging in if auto generation is disabled
+if(!method.AutoGenerateLoginCodes())
+	{
+    refloow.logOn(LogOnOptionsMANUAL);
+}
+    
+
+// Event when logged in
+refloow.on('loggedOn', () => {
+    // Geting persona
+	refloow.getPersonas([refloow.steamID], (personas) => {
+        // DEV console display message after login
+		infolog.correct(`| [Refloow] | LOGIN |: Logined to Steam as ${personas[refloow.steamID].player_name} \n\n`);
+
+
+	    console.log('Everything is ready: '.cyan);
+    if(method.ChatLogsForEachUserEnabled()) {
+    console.log('Saving logs FOR each user. -Enabled'.cyan);
+    } else {
+    console.log('Saving logs FOR each USER. -Disabled')
+    }
+    if(method.DailyChatLogsEnabled()) {
+    console.log('Saving logs FOR each DAY.  -Enabled'.cyan);
+    } else {
+    console.log('Saving logs FOR each DAY.  -Disabled \n')
+    }
+
+    if (!method.DailyChatLogsEnabled() && !method.ChatLogsForEachUserEnabled()) {
+    console.log('Looks like both chat logs are set to false... Set at least one for each DAY or USER to true for client to save logs.')
+    }
+ });
+});
+
+// Loging of messages
+refloow.on("friendMessage", function (steamID, message) {
+  if (userLogs[steamID.getSteamID64()]) {
+        userLogs[steamID.getSteamID64()].push(message);
+    } else {
+        userLogs[steamID.getSteamID64()] = [];
+        userLogs[steamID.getSteamID64()].push(message);
+    }
+    if(method.ChatLogsForEachUserEnabled()) {
+        fs.writeFile("./Settings/ChatLogs/UserLogs/" + steamID.getSteamID64() + "-log-" + new Date().getDate() + "-" + new Date().getMonth() + "-" + new Date().getFullYear() + ".json", JSON.stringify({logs: userLogs[steamID.getSteamID64()]}), (ERR) => {
+            if (ERR) {
+                infolog.error("| |UserData| |: An error occurred while writing UserLogs file: " + ERR);
+            }
+        });
+    }
+    if(method.DailyChatLogsEnabled()) {
+        chatLogs += steamID.getSteamID64() + " : " + message + "\n";
+        fs.writeFile("./Settings/ChatLogs/FullLogs/log-" + new Date().getDate() + "-" + new Date().getMonth() + "-" + new Date().getFullYear() + ".txt", chatLogs, (ERR) => {
+            if (ERR) {
+                infolog.error("| |UserData| |: An error occurred while writing FullLogs file: " + ERR);
+            }
+        });
+    }
+    if (userMsgs[steamID.getSteamID64()]) {
+        userMsgs[steamID.getSteamID64()]++;
+    } else {
+        userMsgs[steamID.getSteamID64()] = 1;
+    }
+});
+
+// Steam Chat Logger - Bot built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowlibrarycontact@gmail.com
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+ */
---
+++ app/infolog.js
@@ -0,0 +1,61 @@
+// Steam Chat Logger - Bot built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowlibrarycontact@gmail.com
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+ */
+
+// This file is not part of the bot here is writed code for cmd color log.
+
+try {
+    // Checking if module winston is correctly installed
+    winston = require('winston');
+} catch (ex) {
+    // If modules are not installed showing an clear error message to user.
+    console.log('| [Modules] |: Missing dependencies. Install a version with dependecies or use npm install.');
+    console.log(ex);
+    process.exit(1);
+}
+
+const level ={
+  error: 0,
+  correct: 1,
+  fail: 2,
+  new: 3,
+  info: 4,
+  give: 5,
+  receive: 6
+};
+const colors = {
+    error: 'bold red',
+    correct: 'bold green',
+    fail: 'red',
+    new: 'underline green',
+    info: 'yellow',
+    give: 'underline red',
+    receive: 'underline green'};
+
+winston.addColors(colors);
+const logger = module.exports = winston.createLogger({
+  levels: level,
+  format: winston.format.combine(
+    winston.format.colorize({message: true}),
+    winston.format.timestamp({
+      format: 'HH:mm:ss'
+    }),
+    winston.format.printf(info => `${info.timestamp}: ${info.message}`)
+  ),
+  transports: [
+    new winston.transports.Console()
+  ],
+  level: 'info'
+});
+
+// Steam Chat Logger - Bot built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowlibrarycontact@gmail.com
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+ */
