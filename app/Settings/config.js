7085fb034595533ce97d7b46c28f2c6fcd3c7fe0
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-01-03 01:26:39 +0100

    Added note

--- app/Settings/config.js
+++ app/Settings/config.js
@@ -1,15 +1,29 @@
 module.exports = {
 
 // Steam Chat Logger - Bot built by Refloow (-MajokingGames)
+	
+	    
+/*
+
+Want active support and new updates with new features all for free?
+
+Leave an star on github repo its free ( we push updates based on the engagement )
+Repo link: https://github.com/OSL-Works/Steam-Chat-Logger
+
+  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
+
+*/
+	
 
 /* 
   Here is contact info: refloowlibrarycontact@gmail.com
   main dev steam: https://steamcommunity.com/id/MajokingGames/
-  Support Discord: https://discord.gg/nNSDPvKBjt
   
   Donate: https://ko-fi.com/refloow
 
  */
+
+
 
 
     // Account Information //
