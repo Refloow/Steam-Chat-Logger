3c9e1631cd2dc6e8bd365849bb8f91c69891d53b
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-14 16:30:15 +0100

    Added options for 1.0.1

--- app/Settings/config.js
+++ app/Settings/config.js
@@ -1,6 +1,8 @@
 module.exports = {
 
-// Steam Chat Logger - Bot built by Refloow (-MajokingGames)
+
+/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Chat-Logger*/
 	
 	    
 /*
@@ -32,11 +34,12 @@ Repo link: https://github.com/OSL-Works/Steam-Chat-Logger
 	password: '',
 	// Leave empty if account dont have 2fa auth. // If you leave shared_secret empty configure steamguardauto false to manualy enter codes.
 	shared_secret: '',
-  SteamGuardAuto: true,     // Enable or disable | [true / false] automaticly generating 2fa codes for loging into account.
+        SteamGuardAuto: true,     // Enable or disable | [true / false] automaticly generating 2fa codes for loging into account.
 
           // Settings //
           
         UpdateNotif_enable: true,               // [true/false] Enable or disable | Dev logs update notifications
+        ReceivedMessagesDisplay: true,          // [true/false] Enable or disable | Disable received messages console display
 
         LogsForEachUser: true,                  // [true/false] Enable or disable | Disabling logging for each user
         LogsForEachDay: true,                   // [true/false] Enable or disable | Disabling logging for each day
