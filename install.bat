10a64a90073cec7da7cfc8397fcc40987123d133
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-11-02 16:55:04 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -13,7 +13,7 @@ echo .
 echo // Copyright notice:
 echo .
 echo /* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
-echo  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Chat-Logger */
+echo  Code origin (Free GitHub publish): https://github.com/Refloow/Steam-Chat-Logger */
 echo .
 echo .
 explorer "https://discord.gg/D8WCtDD"
