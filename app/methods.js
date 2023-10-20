aed0e3f6bde05d7aea8a01a03297c1388ab7f59f
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2023-10-20 09:08:48 +0200

    Update methods.js

--- app/methods.js
+++ app/methods.js
@@ -2,7 +2,7 @@
 // Copyright notice:
 
 /*--------------------------------------------------------------------------------------------- 
-* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+* Original work: Copyright (c) 2020-2021-2022-2023 Refloow All rights reserved.
 
 * Code origin: https://github.com/Refloow/Steam-Chat-Logger
 * Developer name: Veljko Vuckovic
@@ -108,5 +108,5 @@ t = module.exports = {
 }
 
 
-/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+/* Original work: Copyright (c) 2020-2021-2022-2023 Refloow All rights reserved.
   Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Chat-Logger*/
